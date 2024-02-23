da1912ff5b89be522c0efbe2650b120364b6c78e
Jack <jack.chin520@gmail.com>
2024-02-23 01:01:08 -0500

    updated dockerfile, added server.py, added index.html, added styles.css, added readme.jpg :)

---
+++ server.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, make_response, render_template
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"]) 
+@app.route("/public/index.html", methods=["GET"])
+def home():
+    with open("/public/index.html", "rb") as file:
+        readBytes = file.read()
+    return make_response(readBytes, "HTTP/1.1 200 OK", [("Content-Type", "text/html"), ("X-Content-Type-Options", "nosniff")])
+
+@app.route("/public/styles.css")
+def styles():
+    with open("/public/styles.css", "rb") as file:
+        readBytes = file.read()
+    return make_response(readBytes, "HTTP/1.1 200 OK", [("Content-Type", "text/css"), ("X-Content-Type-Options", "nosniff")])
+
+@app.route("/public/image/readme.jpg")
+def retrieve_image(): #* retrieve images
+    with open("/public/image/readme.jpg", "rb") as file:
+        readBytes = file.read()
+    return make_response(readBytes, "HTTP/1.1 200 OK", [("Content-Type", "image/jpeg"), ("X-Content-Type-Options", "nosniff")])
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return make_response(b"Can't find page", "HTTP/1.1 404 Not Found", [("Content-Type", "text/plain"), ("X-Content-Type-Options", "nosniff")])
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
+    
+
+    
