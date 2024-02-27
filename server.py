1706b9f4809edd2f8217915588d6b384d76d14d5
Jack Chin <jkchin2@buffalo.edu>
2024-02-27 17:36:47 -0500

    Created interactive register page, links back to login

--- server.py
+++ server.py
@@ -1,12 +1,19 @@
-from flask import Flask, request, make_response, render_template
+from flask import Flask, request, make_response, redirect, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='public')
+
+#* -------------------------- GET REQUESTS ------------------------------
 
 @app.route("/", methods=["GET"]) 
 @app.route("/public/index.html", methods=["GET"])
 def home():
     with open("/public/index.html", "rb") as file:
         readBytes = file.read()
+    return make_response(readBytes, "HTTP/1.1 200 OK", [("Content-Type", "text/html"), ("X-Content-Type-Options", "nosniff")])
+
+@app.route("/signup")
+def signup():
+    readBytes = render_template("signup.html", error = "")
     return make_response(readBytes, "HTTP/1.1 200 OK", [("Content-Type", "text/html"), ("X-Content-Type-Options", "nosniff")])
 
 @app.route("/public/styles.css")
@@ -24,6 +31,37 @@ def retrieve_image(): #* retrieve images
 @app.errorhandler(404)
 def page_not_found(error):
     return make_response(b"Can't find page", "HTTP/1.1 404 Not Found", [("Content-Type", "text/plain"), ("X-Content-Type-Options", "nosniff")])
+
+#* -------------------------------- POST REQUESTS ----------------------------------------
+
+@app.route("/register", methods=["POST"])
+def handleSignUp():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    passwordCheck = request.form.get('passwordCheck')
+
+    #TODO: check if username in database
+
+    #TODO: placeholder code
+    '''
+    if username in "database":
+        return "<h1>Username already taken.</h1>"
+    '''
+    if password != passwordCheck:
+        return render_template("signup.html", error = "Passwords are not the same")
+    elif not password and not passwordCheck:
+        return render_template("signup.html", error = "Password and repeated password missing")
+    elif not password:
+        return render_template("signup.html", error = "Password missing")
+    elif not passwordCheck:
+        return render_template("signup.html", error = "Repeated password missing")
+    else:
+        #TODO: put salted hash of password into database
+        #TODO: create auth token, store hashed auth token in database
+
+        response = make_response(redirect("/", code=302))
+        response.set_cookie('authtoken', "placeholder", 3600, httponly = True) #TODO: change "placeholder" to auth token to give user the cookies.
+        return response #* redirect back to homepage
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
