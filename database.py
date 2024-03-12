9bfeff0b382bcd97155977c67e82f95edd7437b7
Nucherino <pennacchiodominic@gmail.com>
2024-03-12 16:08:12 -0400

    added 2 new databases for id and posts

--- database.py
+++ database.py
@@ -3,3 +3,5 @@ from pymongo import MongoClient
 mongo_client = MongoClient("mongo")
 db = mongo_client["cse312"]
 user_login = db["user_login"]
+posts_collection = db["posts"]
+id_collection = db["post_ids"]
