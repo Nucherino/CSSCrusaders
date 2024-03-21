c870a02e87164cfa9db81393ec7e45902b4a7c41
Cody Reynolds <codyrey@buffalo.edu>
2024-03-21 09:25:56 -0400

    added js effect and cleaned up auth page front-end

--- public/app.js
+++ public/app.js
@@ -1,14 +1,3 @@
-function sendPost() {
-    const postBox = document.getElementById("chat-text-box");
-    const message = postBox.value;
-    postBox.value = "";
-    const request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-            console.log(this.response);
-        }
-    }
-    request.open("POST", "/posts");
-    request.send(JSON.stringify(messageJSON));
-    postBox.focus();
+function addText () {
+    document.getElementById("post-title").innerHTML += "Posts";
 }
