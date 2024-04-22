f359a33fef646117141515bd2737d68928563d80
Jack Chin <jkchin2@buffalo.edu>
2024-04-22 04:08:05 -0400

    Revert "wss...?"
    This reverts commit 415d02fa66c43c74b92c0c87e9c118efe1c0ec4b.

--- public/app.js
+++ public/app.js
@@ -10,10 +10,6 @@ function initWS(){
   console.log("HI THERE");
   socket = io("https://csscrusaders.com", {
     //path: "/",
-    cors: {
-      origin: "https://csscrusaders.com",
-      methods: ["GET", "POST"]
-    }, 
     transports: ["websocket", "polling"]
   });
   console.log(socket);
