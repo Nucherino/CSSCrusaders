761b0bf079c89b3be515128b32e81ad58fbfdcd5
Jack Chin <jkchin2@buffalo.edu>
2024-04-22 02:22:19 -0400

    debugging

--- public/app.js
+++ public/app.js
@@ -32,6 +32,17 @@ function initWS(){
     updateLikes(likeData);
     console.log("user liked message");
   })
+
+  socket.on("connect_error", (err) => {
+    // the reason of the error, for example "xhr poll error"
+    console.log(err.message);
+  
+    // some additional description, for example the status code of the initial HTTP response
+    console.log(err.description);
+  
+    // some additional context, for example the XMLHttpRequest object
+    console.log(err.context);
+  });
 }
 
 function updateLikes(likeData) {
